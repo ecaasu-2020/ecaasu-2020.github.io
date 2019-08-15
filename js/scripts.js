fc5442e2ffffb76c01f4f6ed6c281038049b8f0f
Sean Park <seanpark1107@gmail.com>
2019-08-16 03:13:52 +0900

    functioning filtered workshop list

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $('.nav-toggle').click(function() {
 });
 
 //// Hide Header on on scroll down
+//// https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
 
 var didScroll;
 var lastScrollTop = 0;
@@ -44,3 +45,79 @@ function hasScrolled() {
     
     lastScrollTop = st;
 }
+
+
+//// Workshop Search
+
+var selectedBlocks = ["1","2","3"];
+var selectedTracks = [];
+
+function updateWorkshopListDOM() {
+
+	var block;
+	var track;
+
+	$(".workshop").each(function() {
+		block = $(this)[0].getAttribute("data-block");
+		track = $(this)[0].getAttribute("data-track");
+		if(selectedBlocks.includes(block) || selectedTracks.includes(track)) {
+			$(this).removeClass("not-displayed");
+		} else {
+			$(this).addClass("not-displayed");
+		}
+	});
+}
+
+$( document ).ready(function() {
+	if($("#workshops-header").length) {
+		// read all track tags from DOM into selectedTracks
+		$("#track-filter-section > .tag").each(function(index, value) {
+			selectedTracks.push($(this).text());
+		});
+
+		updateWorkshopListDOM();
+	}
+});
+
+$("#workshops-search-filters .filter-tag").click(function(e) {
+	
+	let value = $(this).text();
+
+	if($(this).hasClass("block-tag")) {
+		if(selectedBlocks.indexOf(value) > -1) {
+			selectedBlocks.splice(selectedBlocks.indexOf(value), 1);
+		} else {
+			selectedBlocks.push(value);
+		}
+	} else {
+		if(selectedTracks.indexOf(value) > -1) {
+			selectedTracks.splice(selectedTracks.indexOf(value), 1);
+		} else {
+			selectedTracks.push(value);
+		}
+	}
+
+	$(this).toggleClass('unselected');
+
+	// console.log(selectedBlocks);
+	// console.log(selectedTracks);
+
+	updateWorkshopListDOM();
+});
+
+$("#workshops-deselect-all").click(function(e) {
+	selectedBlocks = [];
+	selectedTracks = [];
+
+	$(".filter-tag").each(function() {
+		$(this).addClass("unselected");
+	});
+
+	// console.log(selectedBlocks);
+	// console.log(selectedTracks);
+
+	updateWorkshopListDOM();
+});
+
+
+
