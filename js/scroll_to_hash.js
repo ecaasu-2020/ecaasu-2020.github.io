19ab412dfdd8dad85dd51e9ecb81124af3dd210d
Sean Park <seanpark1107@gmail.com>
2019-09-06 15:42:07 -0500

    Fixes #4

--- js/scroll_to_hash.js
+++ js/scroll_to_hash.js
@@ -1,7 +1,8 @@
 // Scroll to home section
 
 
-$('.home-section-link').click(function() {
+$('.home-section-link').click(function(e) {
+	e.preventDefault();
 	let target = $(this).prop('hash');
 	$('html, body').animate({
 		scrollTop: $(target).offset().top
