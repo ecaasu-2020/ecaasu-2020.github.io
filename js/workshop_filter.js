47a9d0db7f07bc5c840ded30e6a08e0340b543a7
Sean Park <seanpark1107@gmail.com>
2019-09-07 11:34:03 -0500

    Closes #5; Closes #11

--- js/workshop_filter.js
+++ js/workshop_filter.js
@@ -1,17 +1,18 @@
 //// Workshop Search
 
-var selectedBlocks = ["1","2","3"];
-var selectedTracks = [];
+var selectedBlock = "1"
+// var selectedTracks = [];
 
 function updateWorkshopListDOM() {
 
 	var block;
-	var track;
+	// var track;
 
 	$(".workshop").each(function() {
 		block = $(this)[0].getAttribute("data-block");
-		track = $(this)[0].getAttribute("data-track");
-		if(selectedBlocks.includes(block) && selectedTracks.includes(track)) {
+		// track = $(this)[0].getAttribute("data-track");
+		if(block === selectedBlock) {
+		// if(selectedBlocks.includes(block) && selectedTracks.includes(track)) {
 			$(this).removeClass("not-displayed");
 		} else {
 			$(this).addClass("not-displayed");
@@ -20,52 +21,54 @@ function updateWorkshopListDOM() {
 }
 
 $( document ).ready(function() {
-	if($("#workshops-header").length) {
-		// read all track tags from DOM into selectedTracks
-		$("#track-filter-section > .tag").each(function(index, value) {
-			selectedTracks.push($(this).text());
-		});
 
-		updateWorkshopListDOM();
-	}
+// 	if($("#workshops-header").length) {
+// 		// read all track tags from DOM into selectedTracks
+// 		$("#track-filter-section > .tag").each(function(index, value) {
+// 			selectedTracks.push($(this).text());
+// 		});
+
+	updateWorkshopListDOM();
+// 	}
 });
 
 $("#workshops-search-filters .filter-tag").click(function(e) {
 	
 	let value = $(this).text();
+	selectedBlock = value;
 
-	if($(this).hasClass("block-tag")) {
-		if(selectedBlocks.indexOf(value) > -1) {
-			selectedBlocks.splice(selectedBlocks.indexOf(value), 1);
-		} else {
-			selectedBlocks.push(value);
-		}
-	} else {
-		if(selectedTracks.indexOf(value) > -1) {
-			selectedTracks.splice(selectedTracks.indexOf(value), 1);
-		} else {
-			selectedTracks.push(value);
-		}
-	}
+	// if($(this).hasClass("block-tag")) {
+	// 	if(selectedBlocks.indexOf(value) > -1) {
+	// 		selectedBlocks.splice(selectedBlocks.indexOf(value), 1);
+	// 	} else {
+	// 		selectedBlocks.push(value);
+	// 	}
+	// }
+	// } else {
+	// 	if(selectedTracks.indexOf(value) > -1) {
+	// 		selectedTracks.splice(selectedTracks.indexOf(value), 1);
+	// 	} else {
+	// 		selectedTracks.push(value);
+	// 	}
+	// }
 
-	$(this).toggleClass('unselected');
+	// $(this).toggleClass('unselected');
 
-	// console.log(selectedBlocks);
-	// console.log(selectedTracks);
-
-	updateWorkshopListDOM();
-});
-
-$("#workshops-deselect-all").click(function(e) {
-	selectedBlocks = [];
-	selectedTracks = [];
-
-	$(".filter-tag").each(function() {
-		$(this).addClass("unselected");
+	$("#workshops-search-filters .filter-tag").each(function() {
+		$(this).removeClass('selected');
 	});
-
-	// console.log(selectedBlocks);
-	// console.log(selectedTracks);
+	$(this).addClass('selected');
 
 	updateWorkshopListDOM();
 });
+
+// $("#workshops-deselect-all").click(function(e) {
+// 	selectedBlocks = [];
+// 	selectedTracks = [];
+
+// 	$(".filter-tag").each(function() {
+// 		$(this).addClass("unselected");
+// 	});
+
+// 	updateWorkshopListDOM();
+// });
