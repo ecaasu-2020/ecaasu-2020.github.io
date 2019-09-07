1bf0b888d2a6534d4bbf3f3f2e0c04db5a6b8b9b
Sean Park <seanpark1107@gmail.com>
2019-09-07 08:21:08 -0500

    added logistics section and fixed scroll links interaction

--- js/scroll_to_hash.js
+++ js/scroll_to_hash.js
@@ -1,10 +1,25 @@
 // Scroll to home section
 
-
 $('.home-section-link').click(function(e) {
 	e.preventDefault();
-	let target = $(this).prop('hash');
+	let page = $(this).attr('href').split('#')[0];
+	if(page !== window.location) {
+		window.location = page;
+	}
+	sessionStorage.setItem('hash', '#' + $(this).attr('href').split('#')[1]);
 	$('html, body').animate({
-		scrollTop: $(target).offset().top
+		scrollTop: $(hash).offset().top
 	}, 750);
+
+	location.href.replace(hash, "");
 });
+
+var hash = sessionStorage.getItem('hash')
+
+if(hash !== ""){
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, 750);
+
+	var hash = sessionStorage.setItem('hash', '')
+}
